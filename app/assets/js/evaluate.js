cbbf8532c883149bcf8520157d1920545d32db8f
callumacrae <callum@lynxphp.com>
2014-02-24 18:38:32 +0000

    Fixed bug where "var a = 'b'" would output the previous value.

--- app/assets/js/evaluate.js
+++ app/assets/js/evaluate.js
@@ -9,6 +9,10 @@ define(function () {
 	 * @todo: Make eval call more safe
 	 */
 	var evaluate = function (code) {
+		if (code.indexOf('var') !== -1 || code.indexOf('function') !== -1) {
+			code = 'undefined;' + code;
+		}
+
 		var newCode = codeSoFar + '\n;' + code;
 
 		// If code works, add it to codeSoFar so that variables are preserved
